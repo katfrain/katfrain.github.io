1453ec9ab8198dcaa983583e71f881915fd3875a
katfrain <100097599+katfrain@users.noreply.github.com>
2024-05-29 17:40:56 -0400

    Added some projects to programming page

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const caption = document.getElementById("caption");
   const title = document.getElementById("overlay-title");
   const info = document.getElementById("overlay-info");
+  const olink = document.getElementById("optional-link");
 
 // declaring variables
   let dot = [];
@@ -20,28 +21,40 @@ document.addEventListener("DOMContentLoaded", function() {
           captions: ["Home page design done in Figma", "Different iterations of the wesbite design - also done in Figma"],
           dots: ["dot1", "dot2"],
           projectTitle : ["Personal Website, 2024"],
-          projectInfo : ["I had a lot of fun designing and implementing my personal website. I went through multiple design iterations before I created something that I was satisfied with and felt like it represented my personality. I am very happy with the final design, but I will keep this website as a constant work in progress so that I can continue adding new things and polishing the old designs. <br><br> The most challenging part of this project was implementing the JavaScript, which I used to make these pop-up overlays that showcase my work. I only had very basic knowledge of JavaScript before this project and I feel much more confident with it now. <br><br> Skills used: HTML, CSS, JavaScript, Figma"]
+          projectInfo : ["I had a lot of fun designing and implementing my personal website. I went through multiple design iterations before I created something that I was satisfied with and felt like it represented my personality. I am very happy with the final design, but I will keep this website as a constant work in progress so that I can continue adding new things and polishing the old designs. <br><br> The most challenging part of this project was implementing the JavaScript, which I used to make these pop-up overlays that showcase my work.It had been a while since I had used JavaScript and this project was a nice refresher. <br><br> Skills used: HTML, CSS, JavaScript, Figma"],
+          optionalLink : [""]
       },
       project2: {
           images: ["images/project2-1.png", "images/project2-2.png", "images/project2-3.png", "images/project2-4.png"],
           captions: ["The three posters", "Orthongonal", "Diagonal", "Freeform"],
           dots: ["dot1", "dot2", "dot3", "dot4"],
           projectTitle : ["Mock Poster Design, 2023"],
-          projectInfo : ["Three posters I designed for an assignment at Concordia University. The first one had to be designed based on an orthogonal grid, the second one based on a diagonal grid, and the third one free from. We had a lot of freedom surrounding the subject of our posters, so I created a fake music festival featuring some of my favorite riot grrrl bands.<br><br>Skills used: Figma, Illustrator"]
+          projectInfo : ["Three posters I designed for an assignment at Concordia University. The first one had to be designed based on an orthogonal grid, the second one based on a diagonal grid, and the third one free from. We had a lot of freedom surrounding the subject of our posters, so I created a fake music festival featuring some of my favorite riot grrrl bands.<br><br>Skills used: Figma, Illustrator"],
+          optionalLink : [""]
       },
       project3: {
         images: ["images/project3-3.png", "images/project3-2.png", "images/project3-1.png"],
           captions: ["Final album design", "Moodboard", "Different ideas I had for the cover"],
           dots: ["dot1", "dot2", "dot3"],
           projectTitle : ["Mock Album Design, 2023"],
-          projectInfo : ["A mock album cover designed for an assignment at Concordia University. I really wanted to go a very illustrative route with a painting as the main focus of the cover. You can see the different iterations I went through on slide 3, as well as a mood board I put together to guide my work on slide 2. I created the painting in procreate and put the final design together in Figma.<br><br>Skills used: Procreate, Figma"]
+          projectInfo : ["A mock album cover designed for an assignment at Concordia University. I really wanted to go a very illustrative route with a painting as the main focus of the cover. You can see the different iterations I went through on slide 3, as well as a mood board I put together to guide my work on slide 2. I created the painting in procreate and put the final design together in Figma.<br><br>Skills used: Procreate, Figma"],
+          optionalLink : [""]
       },
       project4: {
         images: ["images/project4-1.png", "images/project4-2.png", "images/project4-4.png"],
         captions: ["Final version of the ticket", "First version of the ticket", "The banner"],
         dots: ["dot1", "dot2", "dot3"],
         projectTitle : ["Design work for school event, 2022"],
-        projectInfo : ["I was asked to design a ticket and a banner for my High School’s reunion/grad do-over, as our grad was canceled by the Pandemic. I had a lot of fun transforming our school’s logo and colors in my design.<br><br>This was my first real graphic design project and it taught me a lot about vector editing, color choice, and making sure my design is practical for real life use.<br><br>Skills used: Illustrator"]
+        projectInfo : ["I was asked to design a ticket and a banner for my High School’s reunion/grad do-over, as our grad was canceled by the Pandemic. I had a lot of fun transforming our school’s logo and colors in my design.<br><br>This was my first real graphic design project and it taught me a lot about vector editing, color choice, and making sure my design is practical for real life use.<br><br>Skills used: Illustrator"],
+        optionalLink : [""]
+    },
+    project5: {
+      images: ["images/project5-2.gif", "images/project5-3.png"],
+      captions: ["Our little study knight in action!", "The chrome extension"],
+      dots: ["dot1", "dot2"],
+      projectTitle : ["StudyKnight, 2024"],
+      projectInfo : ["StudyKnight is a Hackathon project I collaborated on with 2 other students. We created a chrome extension that acts as a game, a time tracker, and a personal checklist, as well as taking advantage of your own personal reward systems to keep you doing what's best for you. Using our own xp and level up system, completing tasks, staying on productive websites, and doing long sessions of work, gives you and your knight xp, eventually leveling up to grant you bigger and better weapons. You also add to your own list of distracting websites, which will decrease your xp, to encourage you to stay on track<br><br>I was in charge of the pixel art assets, UI design, and implementing this using html and css. This project pushed me out of my comfort zone creatively and I am really proud of what I have accomplished with Study Knight!<br><br>Skills used: HTML, CSS, Aseprite, Figma<br><br>"],
+      optionalLink : ["https://devpost.com/software/study-knight"]
     }
   };
 
@@ -54,6 +67,8 @@ document.addEventListener("DOMContentLoaded", function() {
           currentProject = projects[projectKey];
           title.innerHTML = currentProject.projectTitle[0];
           info.innerHTML = currentProject.projectInfo[0];
+          olink.innerHTML = currentProject.optionalLink;
+          olink.href = currentProject.optionalLink;
           currentProject.dots.forEach((dotId, index) => {
               dot.push(dotId);
               document.getElementById(dotId).style.display = "inline-block";
